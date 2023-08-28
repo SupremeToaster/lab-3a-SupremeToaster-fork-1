03f870d3a193e171b41d7f51cff8b6049cc73519
Tao <taoyang.peter@gmail.com>
2023-08-28 16:08:26 -0600

    db_connection and index

--- src/index.php
+++ src/index.php
@@ -4,7 +4,6 @@ session_start();
 
 // Check if the user is logged in
 if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== 'yes') {
-    // Redirect to login page if not logged in
     header("Location: views/login.php");
     exit;
 }

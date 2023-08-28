9c88867738ff236b6cbbea2d17c20bfd54075200
Tao <taoyang.peter@gmail.com>
2023-08-28 16:08:45 -0600

    db_connection and index

--- src/index.php
+++ src/index.php
@@ -4,6 +4,10 @@ session_start();
 
 // Check if the user is logged in
 if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== 'yes') {
+    // Redirect to login page if not logged in
+    // Redirect to login page if not logged in
+    // Redirect to login page if not logged in
+    // Redirect to login page if not logged in
     header("Location: views/login.php");
     exit;
 }
@@ -34,28 +38,25 @@ function echoTask($task) {
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="css/style.css">
-  <!-- Add an appropriate title in this tag -->
+  <title>Lab-3A</title>
   <title>
     Lab-3A
   </title>
-  <!-- Links to stylesheets -->
-</head>
-
-<body>
-  <!-- Your visible elements -->
-  <nav class ="navbar">
+  <nav class="navbar">
+    <a href="https://707d8d6bdb434516a0857a9cc637bec2.vfs.cloud9.us-west-1.amazonaws.com/_static/public_html/portfoliolab/home.html">My Resume</a>
     <a class=""
       href="https://707d8d6bdb434516a0857a9cc637bec2.vfs.cloud9.us-west-1.amazonaws.com/_static/public_html/portfoliolab/home.html">My
       Resume</a>
       <!-- Other navigation links -->
     <a href="actions/logout_action.php">Log Out</a>
   </nav>
+    <?php
+    while ($row = $result->fetch_assoc()) {
+        echoTask($row);
+    }
+    ?>
   <h1>My To-Do</h1>
-  <input type="checkbox" class="toggle-switch" id="cb-sort" /><label for="cb-sort">Sort by date</label>
+  <form class="form-create-task" action="actions/create_action.php" method="post">
   <input type="checkbox" class="toggle-switch" id="cb-filter" /><label for="cb-filter">Filter completed tasks</label>
   <ul id="taskContainer" class="tasklist">
   </ul>
@@ -63,9 +64,11 @@ function echoTask($task) {
     <input type="text" name="description" required class="my-input" /><br>
     <input type="date" name="date" required class="my-input" /><br>
     <button class="button-styled">Create Task</button><br>
-  </form>
+    
   <!-- Links to scripts -->
-  <script src="js/script.js"></script>
-</body>
-
-</html>
+<?php
+function echoTask($task) {
+    // Your HTML code to display each task
+    echo '<li>' . $task['description'] . ' - ' . $task['date'] . '</li>';
+}
+?></html></html></html></html>

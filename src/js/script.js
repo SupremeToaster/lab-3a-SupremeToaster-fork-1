e0f34677051f91d619bd86dce366eaaf21f84b73
Tao Yang <taoyang.peter@gmail.com>
2023-09-01 17:41:22 -0600

    lab 3 complete

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,28 @@
 // script.js
 function deleteTask(taskId) {
-  $.post("actions/delete_action.php", { task_id: taskId }, function(data) {
+  $.post("actions/delete_action.php", { task_id: taskId }, function (data) {
     // Remove the deleted task from the DOM or refresh the task list
     $("#task-" + taskId).remove();
   });
 }
 
 function updateTask(taskId) {
-  $.post("actions/update_action.php", { task_id: taskId }, function(data) {
-    const taskElement = $("#task-" + taskId);
-    const descriptionElement = taskElement.find(".task-description");
-    
-    if (data.success) {
-      // Toggle the 'task-checked' class
-      descriptionElement.toggleClass("task-checked");
-    } else {
-      console.error("Failed to update task:", data.error);
-    }
-  }, "json");  // Expect a JSON response
+  $.post(
+    "actions/update_action.php",
+    { task_id: taskId },
+    function (data) {
+      const taskElement = $("#task-" + taskId);
+      const descriptionElement = taskElement.find(".task-description");
+
+      if (data.success) {
+        // Toggle the 'task-checked' class
+        descriptionElement.toggleClass("task-checked");
+      } else {
+        console.error("Failed to update task:", data.error);
+      }
+    },
+    "json"
+  ); // Expect a JSON response
 }
 
 /*

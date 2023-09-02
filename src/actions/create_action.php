6300f785540e250bd500949086d3212ec686f993
Tao Yang <taoyang.peter@gmail.com>
2023-09-02 17:14:11 -0600

    createaction

--- src/actions/create_action.php
+++ src/actions/create_action.php
@@ -1,18 +1,27 @@
 <?php
 session_start();
-include 'db_connection.php'; // Assume you have a db_connection.php file
+include 'db_connection.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $text = $_POST['text'];
     $date = $_POST['date'];
-    $user_id = $_SESSION['user_id']; // Assuming user_id is stored in session
+    $user_id = $_SESSION['user_id'];
+    $done = 0;  // Task not done
 
-    $stmt = $conn->prepare("INSERT INTO tasks (text, date, user_id) VALUES (:text, :date, :user_id)");
+    // Prepare SQL statement
+    $stmt = $conn->prepare("INSERT INTO tasks (user_id, text, date, done) VALUES (:user_id, :text, :date, :done)");
+
+    // Bind parameters
+    $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
     $stmt->bindParam(':text', $text, PDO::PARAM_STR);
     $stmt->bindParam(':date', $date, PDO::PARAM_STR);
-    $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
-    $stmt->execute();
+    $stmt->bindParam(':done', $done, PDO::PARAM_INT);
 
-    header("Location: index.php");
+    // Execute the statement
+    if ($stmt->execute()) {
+        header('Location: ../index.php');
+    } else {
+        echo "Error: " . $stmt->errorInfo()[2];
+    }
 }
 ?>

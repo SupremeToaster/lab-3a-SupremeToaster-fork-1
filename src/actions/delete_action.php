0c00998472ceb6660848e92c659959b4d8a598bd
Tao Yang <taoyang.peter@gmail.com>
2023-09-05 14:23:16 -0600

    delete

--- src/actions/delete_action.php
+++ src/actions/delete_action.php
@@ -5,8 +5,9 @@ include 'db_connection.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $task_id = $_POST['task_id'];
 
-    $stmt = $conn->prepare("DELETE FROM tasks WHERE id = ?");
-    $stmt->bind_param("i", $task_id);
+    $stmt = $conn->prepare("UPDATE tasks SET done = :done WHERE id = :task_id");
+    $stmt->bindParam(':done', $done, PDO::PARAM_INT);
+    $stmt->bindParam(':task_id', $task_id, PDO::PARAM_INT);
     $stmt->execute();
 
     header("Location: ../index.php");

0b46120ec418fba587a90acb1925584ae60c80b3
Tao Yang <taoyang.peter@gmail.com>
2023-09-05 14:32:07 -0600

    delete/update

--- src/actions/delete_action.php
+++ src/actions/delete_action.php
@@ -6,11 +6,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $task_id = $_POST['task_id'];
     $done = isset($_POST['done']) ? 1 : 0;
 
-    $stmt = $conn->prepare("UPDATE tasks SET done = :done WHERE id = :task_id");
-    $stmt->bindParam(':done', $done, PDO::PARAM_INT);
+    $stmt = $conn->prepare("DELETE FROM tasks WHERE id = :task_id");
     $stmt->bindParam(':task_id', $task_id, PDO::PARAM_INT);
     $stmt->execute();
-
+    
     header("Location: ../index.php");
 }
 ?>
--- src/actions/update_action.php
+++ src/actions/update_action.php
@@ -6,8 +6,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $task_id = $_POST['task_id'];
     $done = isset($_POST['done']) ? 1 : 0;
 
-    $stmt = $conn->prepare("UPDATE tasks SET done = ? WHERE id = ?");
-    $stmt->bind_param("ii", $done, $task_id);
+    $stmt = $conn->prepare("UPDATE tasks SET done = :done WHERE id = :task_id");
+    $stmt->bindParam(':done', $done, PDO::PARAM_INT);
+    $stmt->bindParam(':task_id', $task_id, PDO::PARAM_INT);
     $stmt->execute();
 
     header("Location: ../index.php");
